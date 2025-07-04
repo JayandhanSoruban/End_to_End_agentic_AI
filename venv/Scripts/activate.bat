035e44def779359a9f5d0bb741db49bec141dee8
Jayandhan Soruban <lionlike2003@gmail.com>
2025-07-04 16:49:04 +0530

    text splitter

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\SELF LEARNING\venv
+set VIRTUAL_ENV=D:\End_to_End_agentic_AI\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
